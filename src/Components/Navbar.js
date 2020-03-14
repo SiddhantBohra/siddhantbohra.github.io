76083c71f17b407600e7978611a0ee61aea30bf9
Siddhant Bohra <abhisidds@gmail.com>
2020-03-15 01:53:15 +0530

    Updated UI

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,19 +11,19 @@ const NavBar = (props) => {
   return (
     <div>
       <Navbar className = 'navbar' dark expand="md">
-      <NavbarBrand className = "brand" href="/">Siddhant Bohra</NavbarBrand>
+      <NavbarBrand className = "brand" href="/"><i class="fa fa-code" id="facolor" aria-hidden="true">SiddhantBohra</i></NavbarBrand>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <span><NavLink href="/aboutme" >About Me</NavLink></span>
+              <span><NavLink href="#aboutme" >About Me</NavLink></span>
+            </NavItem>
+            <NavItem>
+            <span><NavLink href="/contact">Contact Me</NavLink></span>
             </NavItem>
             <NavItem>
               <span><NavLink href="https://drive.google.com/file/d/13C5GKCWIMiVCE6HLz7_UqneqD_ukvZhO/preview" target = "_blank">Resume</NavLink></span>
             </NavItem>
             <NavItem>
               <span><NavLink href="https://github.com/SiddhantBohra" target = "_blank">GitHub</NavLink></span>
-            </NavItem>
-            <NavItem>
-            <span><NavLink href="/contact">Contact Me</NavLink></span>
             </NavItem>
           </Nav>
       </Navbar>
--- src/Components/landingPage.js
+++ src/Components/landingPage.js
@@ -4,33 +4,38 @@ import aboutMe from './aboutMe'
 export default class landingPage extends Component {
     render() {
         return (
-                <div id='landingPage'>
-                    <Grid className='landing-grid'>
-                        <Cell col={12}>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
-                                alt="avatar"
-                                className="avatar-img"
-                            />
-                            <div className='banner-text'>
-                                <h1>Full Stack Developer</h1>
-                                <hr />
-                                <p>Javascript | Node.JS | Express.JS | MongoDB | AWS | Docker | Kubernetes | React | HTML | CSS</p>
-                                <div className="social-links" >
-                                    <a href="https://www.linkedin.com/in/siddhant-bohra-7a1b0a165/" rel="noopener nereferrer" target="_blank">
-                                        <i className='fa fa-linkedin-square' aria-hidden='true'></i>
-                                    </a>
-                                    <a href="https://github.com/SiddhantBohra" rel="noopener nereferrer" target="_blank">
-                                        <i className='fa fa-github' aria-hidden='true'></i>
-                                    </a>
-                                    <a href="https://www.facebook.com/siddhant.bohra" rel="noopener nereferrer" target="_blank">
-                                        <i className='fa fa-facebook-square' aria-hidden='true'></i>
-                                    </a>
-                                </div>
+            <div id='landingPage'>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img
+                            src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className='banner-text'>
+                            <h1>Full Stack Developer</h1>
+                            <hr />
+                            <p>Javascript | Node.JS | Express.JS | MongoDB | AWS | Docker | Kubernetes | React | HTML | CSS</p>
+                            <div className="social-links" >
+                                <a href="https://www.linkedin.com/in/siddhant-bohra-7a1b0a165/" rel="noopener nereferrer" target="_blank">
+                                    <i className='fa fa-linkedin-square' aria-hidden='true'></i>
+                                </a>
+                                <a href="https://github.com/SiddhantBohra" rel="noopener nereferrer" target="_blank">
+                                    <i className='fa fa-github' aria-hidden='true'></i>
+                                </a>
+                                <a href="https://www.facebook.com/siddhant.bohra" rel="noopener nereferrer" target="_blank">
+                                    <i className='fa fa-facebook-square' aria-hidden='true'></i>
+                                </a>
                             </div>
-                        </Cell>
-                    </Grid>
+                        </div>
+                    </Cell>
+                </Grid>
+                <div id='aboutme'>
+                    <h1>About Me</h1>
+                    <p>Full Stack Web Developer using MongoDB, Express, React, Node.js (MERN) with an emphasis on Backend web development as well as AWS, Docker and Kubernetes, experienced in building applications, RESTful APIs using Node.js and Express, User Auth and OAuth with PassportJS. Common libraries of use: Axios, Mongoose, Lodash, Express, body-parser, socket.io, Bluebird, react-router. Common use of UI libraries: Bootstrap 4, React-Bootstrap, Material-UI, Elemental-UI. </p>
                 </div>
+
+            </div>
         )
     }
 }
