a3fed18d7cd05701860bb19c31d541a8de7ad1e2
Siddhant Bohra <abhisidds@gmail.com>
2020-03-16 15:35:45 +0530

    Update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
-import React  from 'react';
- import {
+import React from 'react';
+import $ from 'jquery'
+import {
   Navbar,
   NavbarBrand,
   Nav,
@@ -8,24 +9,25 @@ import React  from 'react';
 } from 'reactstrap';
 
 const NavBar = (props) => {
+  
   return (
     <div>
-      <Navbar className = 'navbar' dark expand="md" fixed = "top">
-      <NavbarBrand className = "brand" href="/">Siddhant Bohra</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <span><NavLink href="#aboutme" >About Me</NavLink></span>
-            </NavItem>
-            <NavItem>
-            <span><NavLink href="#contact">Contact Me</NavLink></span>
-            </NavItem>
-            <NavItem>
-              <span><NavLink href="https://drive.google.com/file/d/13C5GKCWIMiVCE6HLz7_UqneqD_ukvZhO/preview" target = "_blank">Resume</NavLink></span>
-            </NavItem>
-            <NavItem>
-              <span><NavLink href="https://github.com/SiddhantBohra" target = "_blank">GitHub</NavLink></span>
-            </NavItem>
-          </Nav>
+      <Navbar className='navbar' dark expand="md" fixed="top">
+        <NavbarBrand className="brand" href="/">Siddhant Bohra</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="#aboutme" >About Me</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#contact">Contact Me</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://drive.google.com/file/d/13C5GKCWIMiVCE6HLz7_UqneqD_ukvZhO/preview" target="_blank">Resume</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/SiddhantBohra" target="_blank">GitHub</NavLink>
+          </NavItem>
+        </Nav>
       </Navbar>
     </div>
   );
--- src/Components/landingPage.js
+++ src/Components/landingPage.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { Grid, Cell } from 'react-mdl'
 import aboutMe from './aboutMe'
 import $ from 'jquery'
+import document from 'jquery'
 export default class landingPage extends Component {
-    
     render() {
         return (
             <div id='landingPage'>
@@ -32,7 +32,10 @@ export default class landingPage extends Component {
                         </div>
                     </Cell>
                 </Grid>
-                <aboutMe />
+                <div id='aboutme'>
+                    <h1>About Me</h1>
+                    <p>Full Stack Web Developer using MongoDB, Express, React, Node.js (MERN) with an emphasis on Backend web development as well as AWS, Docker and Kubernetes, experienced in building applications, RESTful APIs using Node.js and Express, User Auth and OAuth with PassportJS. Common libraries of use: Axios, Mongoose, Lodash, Express, body-parser, socket.io, Bluebird, react-router. Common use of UI libraries: Bootstrap 4, React-Bootstrap, Material-UI, Elemental-UI. </p>
+                </div>
             </div>
         )
     }
